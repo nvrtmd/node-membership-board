226ac7323a64c597faf3331c9e7663914f25c97b
YUZAMIN <untmd.crmn@gmail.com>
2022-12-17 01:29:37 +0900

    :sparkles: Member, Post 모델이 정의된 db 객체 생성

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize");
 const path = require("path");
 const env = process.env.NODE_ENV || "development";
+const Member = require("./member");
+const Post = require("./post");
 
 const config = require(path.join(__dirname, "..", "config", "database.js"))[
   env
@@ -12,3 +14,15 @@ const sequelize = new Sequelize(
   config.password,
   config
 );
+
+Member.hasMany(Post);
+Post.belongsTo(Member);
+
+const db = {
+    sequelize,
+    Sequelize,
+    Post: Post(sequelize, Sequelize),
+    Member: Member(sequelize, Sequelize)
+};
+
+module.exports = db;

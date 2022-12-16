d36e5aff6d9e751deff36b352695b3f52f7ce7ac
YUZAMIN <untmd.crmn@gmail.com>
2022-12-17 01:04:35 +0900

    :sparkles: Sequelize 객체 생성

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
+const path = require("path");
+const env = process.env.NODE_ENV || "development";
 
-const sequelize = new Sequelize();
-  
+const config = require(path.join(__dirname, "..", "config", "database.js"))[
+  env
+];
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);

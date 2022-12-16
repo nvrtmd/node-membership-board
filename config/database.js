5d5f29c79d9f23628cd00db894a88b2d706495e8
YUZAMIN <untmd.crmn@gmail.com>
2022-12-17 00:54:26 +0900

    :sparkles: 데이터베이스를 위한 config 파일 생성 및 기본 정보 기입

---
+++ config/database.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const env = env;
+
+const development = {
+  username: env.USER_NAME,
+  password: env.USER_PASSWORD,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  dialect: "mysql",
+  timezone: "+09:00",
+};
+
+const production = {
+  username: env.USER_NAME,
+  password: env.USER_PASSWORD,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  dialect: "mysql",
+};
+
+const test = {
+  username: env.USER_NAME,
+  password: env.USER_PASSWORD,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  dialect: "mysql",
+};
+
+module.exports = { development, production, test };

f08f6746d1ba8f44836a047571f107ee802aeb60
YUZAMIN <untmd.crmn@gmail.com>
2022-12-17 00:48:58 +0900

    :sparkles: app에서 사용할 모듈 use()로 장착

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const Sequelize = require('sequelize');
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(
+    cors({
+      origin: [process.env.FRONT_URL, "http://localhost:3000"],
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    })
+  );
+  
 module.exports = app;

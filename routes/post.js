b10182db8bbb52db58b9aa21fa51e50dbe96b237
YUZAMIN <untmd.crmn@gmail.com>
2022-12-17 18:02:48 +0900

    :sparkles: 게시글 작성 라우터 구현
    회원의 id(primary key)를 포함하여 게시글 작성

--- routes/post.js
+++ routes/post.js
@@ -1,4 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+
+const { isSignedIn } = require("./middlewares");
+
+const { Member, Post } = require("../models/index");
+
+router.post("/create", isSignedIn, async (req, res) => {
+  const token = req.headers.cookie.split("=")[1];
+  const signedinId = jwt.decode(token).memberId;
+
+  const writerId = await Member.findOne({
+    where: { member_id: signedinId },
+  });
+
+  const postData = {
+    post_title: req.body.title,
+    post_contents: req.body.contents,
+    memberId: writerId.id,
+  };
+
+  await Post.create(postData);
+});
 
 module.exports = router;

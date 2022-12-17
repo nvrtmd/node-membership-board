ec8ee7d5ffb46d1d51016355b3abc936db6b6043
YUZAMIN <untmd.crmn@gmail.com>
2022-12-17 15:39:00 +0900

    :sparkles: 회원가입 라우터 생성

--- routes/member.js
+++ routes/member.js
@@ -9,3 +9,19 @@ const { Member } = require("../models/index");
 /**
  * 회원가입
  */
+router.post("/signup", async (req, res, next) => {
+  const encodedPassword = await bcrypt.hash(req.body.password, 12);
+  const newMemberData = {
+    member_id: req.body.id,
+    member_name: req.body.name,
+    member_password: encodedPassword,
+  };
+  await Member.create(newMemberData);
+
+  return res.status(201).json({
+    code: 201,
+    message: "created successfully.",
+  });
+});
+
+module.exports = router;

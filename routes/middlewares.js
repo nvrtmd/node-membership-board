21e6fbd41551427d51a6fd5d1fa0453798e9cc70
YUZAMIN <untmd.crmn@gmail.com>
2022-12-17 15:42:50 +0900

    :sparkles: 아이디 존재 여부를 판단하는 미들웨어 isExistedId 구현

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,1 +1,16 @@
-/** middlewares */
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { Member } = require("../models");
+
+exports.isExistedId = async (req, res, next) => {
+  const memberData = await Member.findOne({ where: { member_id: req.body.id } });
+  if (memberData) {
+    return next();
+  } else {
+    return res.status(404).json({
+      code: 404,
+      message: "id doesn't exist.",
+    });
+  }
+};

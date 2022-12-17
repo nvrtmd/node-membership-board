9e4650ccae4612df9c22c4e0e39762e77a3592bc
YUZAMIN <untmd.crmn@gmail.com>
2022-12-18 02:46:42 +0900

    :sparkles: member 테이블에 paranoid 속성 추가

--- models/member.js
+++ models/member.js
@@ -1,23 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("member", {
-    member_idx: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      allowNull: false,
-      primaryKey: true,
+  return sequelize.define(
+    "member",
+    {
+      member_idx: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      member_id: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      member_password: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      member_nickname: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
     },
-    member_id: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-      unique: true,
-    },
-    member_password: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-    member_nickname: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-    },
-  });
+    {
+      timestamps: true,
+      paranoid: true,
+    }
+  );
 };

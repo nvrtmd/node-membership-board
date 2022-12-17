c2680848fea5a49c20034f22d427518509a505d1
YUZAMIN <untmd.crmn@gmail.com>
2022-12-17 18:13:37 +0900

    :sparkles: 게시글 목록 조회 라우터 구현

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-
 const { isSignedIn } = require("./middlewares");
-
 const { Member, Post } = require("../models/index");
 
+/**
+ * 게시글 목록 조회
+ */
+router.get("/list", async (req, res) => {
+  const postList = await Post.findAll();
+  res.status(200).json({
+    code: 200,
+    data: postList,
+  });
+});
+
+/**
+ * 게시글 생성
+ */
 router.post("/create", isSignedIn, async (req, res) => {
   const token = req.headers.cookie.split("=")[1];
   const signedinId = jwt.decode(token).memberId;

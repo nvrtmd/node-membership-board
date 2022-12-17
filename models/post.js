bee84afcaa1a42a199989f11fba0133ebc2d554f
YUZAMIN <untmd.crmn@gmail.com>
2022-12-17 17:23:36 +0900

    :sparkles: post 모델 수정

--- models/post.js
+++ models/post.js
@@ -2,12 +2,6 @@ module.exports = (sequelize, DataTypes) => {
   return sequelize.define(
     "post",
     {
-      post_id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-        allowNull: false,
-      },
       post_title: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -16,15 +10,12 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      post_display: {
-        type: DataTypes.TINYINT(1),
-        allowNull: true,
-        comment: "게시글 게시 여부 - 1 or 0",
-      },
-      post_register_date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        comment: "게시글 등록 일시",
+      memberId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "members",
+          key: "id",
+        },
       },
     },
     {

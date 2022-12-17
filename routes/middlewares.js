03149478ab2ca6ba2b65466f653caa290719fa8d
YUZAMIN <untmd.crmn@gmail.com>
2022-12-17 15:46:02 +0900

    :sparkles: 비밀번호 일치 여부를 판단하는 미들웨어 isCorrectPassword 구현

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -4,13 +4,35 @@ const jwt = require("jsonwebtoken");
 const { Member } = require("../models");
 
 exports.isExistedId = async (req, res, next) => {
-  const memberData = await Member.findOne({ where: { member_id: req.body.id } });
+  const memberData = await Member.findOne({
+    where: { member_id: req.body.id },
+  });
   if (memberData) {
     return next();
   } else {
     return res.status(404).json({
       code: 404,
       message: "id doesn't exist.",
+    });
+  }
+};
+
+exports.isCorrectPassword = async (req, res, next) => {
+  const memberData = await Member.findOne({
+    where: { member_id: req.body.id },
+  });
+
+  const isCorrectPassword = await bcrypt.compare(
+    req.body.password,
+    memberData.member_password
+  );
+
+  if (isCorrectPassword) {
+    return next();
+  } else {
+    return res.status(404).json({
+      code: 404,
+      message: "password isn't correct.",
     });
   }
 };

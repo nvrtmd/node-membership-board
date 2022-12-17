f322a88c556b8b04821c1c7131eaf9851fa794ff
YUZAMIN <untmd.crmn@gmail.com>
2022-12-17 23:02:16 +0900

    :sparkles: member 테이블의 pk인 id 값을 통해 회원 정보 조회 라우터 구현

--- routes/member.js
+++ routes/member.js
@@ -1,5 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const { isSignedIn } = require("./middlewares");
+const { Member, Post } = require("../models/index");
+
+/**
+ * 회원 정보 조회
+ */
+router.get("/info", isSignedIn, async (req, res) => {
+  const token = req.headers.cookie.split("=")[1];
+  const signedinId = await Member.findOne({
+    where: { member_id: jwt.decode(token).memberId },
+  });
+
+  const signedinMemberInfo = await Member.findByPk(signedinId.id, {
+    attributes: ["member_id", "member_nickname"],
+  });
+
+  res.status(200).json({
+    code: 200,
+    data: signedinMemberInfo,
+  });
+});
 
 module.exports = router;

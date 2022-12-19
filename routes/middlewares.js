b9711edac49c75ed73ac11a24c148464f7e58164
YUZAMIN <untmd.crmn@gmail.com>
2022-12-20 02:52:15 +0900

    :sparkles: 로그인된 사용자가 댓글의 작성자인지 확인하는 middleware 구현

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const { verify } = require("../modules/jwt");
-const { Member, Post } = require("../models");
+const { Member, Post, Comment } = require("../models");
 
 exports.isExistedId = async (req, res, next) => {
   const accordMember = await Member.findOne({
@@ -93,6 +93,31 @@ exports.isPostWriter = async (req, res, next) => {
   if (
     signedInMemberId === postWriter.member_id &&
     signedInMember.member_password === postWriter.member_password
+  ) {
+    return next();
+  }
+  return res.status(403).json({
+    code: 403,
+    message: "forbidden to access.",
+  });
+};
+
+exports.isCommentWriter = async (req, res, next) => {
+  const commentIdx = req.params.commentIdx;
+  const token = req.headers.cookie.split("=")[1];
+  const signedInMemberId = verify(token, process.env.JWT_SECRET_KEY).memberId;
+  const signedInMember = await Member.findOne({
+    where: { member_id: signedInMemberId },
+  });
+  const comment = await Comment.findOne({ where: { comment_idx: commentIdx } });
+  const commentWriterIdx = comment.member_idx;
+  const commentWriter = await Member.findOne({
+    where: { member_idx: commentWriterIdx },
+  });
+
+  if (
+    signedInMemberId === commentWriter.member_id &&
+    signedInMember.member_password === commentWriter.member_password
   ) {
     return next();
   }

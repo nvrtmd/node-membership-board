2a3e15d0781624f18048bb5b1020d1ced73dfe44
YUZAMIN <untmd.crmn@gmail.com>
2022-12-19 20:56:24 +0900

    :sparkles: jwt sign과 verify를 위한 로직을 파일로 분리
    modules/jwt.js에 jwt 핸들링 로직을 모아서 저장

---
+++ modules/jwt.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+exports.sign = async (payload) => {
+  const signedToken = jwt.sign(payload, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRE_TIME,
+    issuer: process.env.JWT_ISSUER,
+  });
+
+  return signedToken;
+};
+
+exports.verify = async (token) => {
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  } catch (err) {
+    if (err.message === "jwt expired") {
+      throw new Error("token was expired. fail to verify.");
+    } else {
+      throw new Error("token is invalid. fail to verify.");
+    }
+  }
+  return decoded;
+};

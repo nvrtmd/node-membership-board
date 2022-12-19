d75dea59e55483c7c499646e3b6b92a16dfb2d6c
YUZAMIN <untmd.crmn@gmail.com>
2022-12-19 22:32:10 +0900

    :sparkles: 게시글 테이블에 post_idx 컬럼(pk) 추가

--- models/post.js
+++ models/post.js
@@ -2,6 +2,12 @@ module.exports = (sequelize, DataTypes) => {
   const Post = sequelize.define(
     "post",
     {
+      post_idx: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
       post_title: {
         type: DataTypes.STRING,
         allowNull: false,

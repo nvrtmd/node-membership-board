2c4ea589303890145817bfe97f0e2f717b590ba5
YUZAMIN <untmd.crmn@gmail.com>
2023-02-01 02:49:43 +0900

    :sparkles: middleware에 try catch문 적용
    적절한 status code를 함께 응답

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -1,38 +1,45 @@
 const bcrypt = require("bcryptjs");
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const { verify } = require("../modules/jwt");
 const { Member, Post, Comment } = require("../models");
 
 exports.isExistedId = async (req, res, next) => {
-  const accordMember = await Member.findOne({
-    where: { member_id: req.body.id },
-  });
-  if (accordMember) {
-    return next();
-  } else {
-    return res.status(404).json({
-      code: 404,
-      message: "id doesn't exist.",
+  try {
+    const accordMember = await Member.findOne({
+      where: { member_id: req.body.id },
     });
+    if (accordMember) {
+      return next();
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
+    }
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
 };
 
 exports.isCorrectPassword = async (req, res, next) => {
-  const accordMember = await Member.findOne({
-    where: { member_id: req.body.id },
-  });
-
-  const isCorrectPassword = await bcrypt.compare(
-    req.body.password,
-    accordMember.member_password
-  );
-
-  if (isCorrectPassword) {
-    return next();
-  } else {
-    return res.status(404).json({
-      code: 404,
-      message: "password isn't correct.",
+  try {
+    const accordMember = await Member.findOne({
+      where: { member_id: req.body.id },
     });
+
+    const isCorrectPassword = await bcrypt.compare(
+      req.body.password,
+      accordMember.member_password
+    );
+
+    if (isCorrectPassword) {
+      return next();
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
+    }
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
 };
 
@@ -44,85 +51,94 @@ exports.isSignedIn = async (req, res, next) => {
       res.locals.token = token;
       next();
     } catch {
-      return res.status(401).json({
-        code: 401,
-        message: "unauthorized user. Need to sign in.",
-      });
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .send(ReasonPhrases.UNAUTHORIZED);
     }
   } catch {
-    return res.status(401).json({
-      code: 401,
-      message: "unauthorized user. Need to sign in.",
-    });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
 };
 
 exports.isAdmin = async (req, res, next) => {
-  const token = req.headers.cookie.split("=")[1];
-  const signedInMemberId = verify(token, process.env.JWT_SECRET_KEY).memberId;
-  const signedInMember = await Member.findOne({
-    where: { member_id: signedInMemberId },
-  });
-  const admin = await Member.findOne({ where: { member_id: "admin" } });
+  try {
+    const token = req.headers.cookie.split("=")[1];
+    const signedInMemberId = verify(token, process.env.JWT_SECRET_KEY).memberId;
+    const signedInMember = await Member.findOne({
+      where: { member_id: signedInMemberId },
+    });
+    const admin = await Member.findOne({ where: { member_id: "admin" } });
 
-  if (
-    signedInMemberId === "admin" &&
-    signedInMember.member_password === admin.member_password
-  ) {
-    return next();
+    if (
+      signedInMemberId === "admin" &&
+      signedInMember.member_password === admin.member_password
+    ) {
+      return next();
+    }
+    return res.status(StatusCodes.FORBIDDEN).send(ReasonPhrases.FORBIDDEN);
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
-  return res.status(403).json({
-    code: 403,
-    message: "forbidden to access.",
-  });
 };
 
 exports.isPostWriter = async (req, res, next) => {
-  const postIdx = req.params.postIdx;
-  const token = req.headers.cookie.split("=")[1];
-  const signedInMemberId = verify(token, process.env.JWT_SECRET_KEY).memberId;
-  const signedInMember = await Member.findOne({
-    where: { member_id: signedInMemberId },
-  });
-  const post = await Post.findOne({ where: { post_idx: postIdx } });
-  const postWriterIdx = post.member_idx;
-  const postWriter = await Member.findOne({
-    where: { member_idx: postWriterIdx },
-  });
+  try {
+    const postIdx = req.params.postIdx;
+    const token = req.headers.cookie.split("=")[1];
+    const signedInMemberId = verify(token, process.env.JWT_SECRET_KEY).memberId;
+    const signedInMember = await Member.findOne({
+      where: { member_id: signedInMemberId },
+    });
+    const post = await Post.findOne({ where: { post_idx: postIdx } });
+    const postWriterIdx = post.member_idx;
+    const postWriter = await Member.findOne({
+      where: { member_idx: postWriterIdx },
+    });
 
-  if (
-    signedInMemberId === postWriter.member_id &&
-    signedInMember.member_password === postWriter.member_password
-  ) {
-    return next();
+    if (
+      signedInMemberId === postWriter.member_id &&
+      signedInMember.member_password === postWriter.member_password
+    ) {
+      return next();
+    }
+    return res.status(StatusCodes.FORBIDDEN).send(ReasonPhrases.FORBIDDEN);
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
-  return res.status(403).json({
-    code: 403,
-    message: "forbidden to access.",
-  });
 };
 
 exports.isCommentWriter = async (req, res, next) => {
-  const commentIdx = req.params.commentIdx;
-  const token = req.headers.cookie.split("=")[1];
-  const signedInMemberId = verify(token, process.env.JWT_SECRET_KEY).memberId;
-  const signedInMember = await Member.findOne({
-    where: { member_id: signedInMemberId },
-  });
-  const comment = await Comment.findOne({ where: { comment_idx: commentIdx } });
-  const commentWriterIdx = comment.member_idx;
-  const commentWriter = await Member.findOne({
-    where: { member_idx: commentWriterIdx },
-  });
+  try {
+    const commentIdx = req.params.commentIdx;
+    const token = req.headers.cookie.split("=")[1];
+    const signedInMemberId = verify(token, process.env.JWT_SECRET_KEY).memberId;
+    const signedInMember = await Member.findOne({
+      where: { member_id: signedInMemberId },
+    });
+    const comment = await Comment.findOne({
+      where: { comment_idx: commentIdx },
+    });
+    const commentWriterIdx = comment.member_idx;
+    const commentWriter = await Member.findOne({
+      where: { member_idx: commentWriterIdx },
+    });
 
-  if (
-    signedInMemberId === commentWriter.member_id &&
-    signedInMember.member_password === commentWriter.member_password
-  ) {
-    return next();
+    if (
+      signedInMemberId === commentWriter.member_id &&
+      signedInMember.member_password === commentWriter.member_password
+    ) {
+      return next();
+    }
+    return res.status(StatusCodes.FORBIDDEN).send(ReasonPhrases.FORBIDDEN);
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
-  return res.status(403).json({
-    code: 403,
-    message: "forbidden to access.",
-  });
 };

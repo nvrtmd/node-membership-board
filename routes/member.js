735ebeee4658932a3b9661f84316de3710833842
YUZAMIN <untmd.crmn@gmail.com>
2023-02-01 02:43:02 +0900

    :sparkles: member 로직에 try catch문 적용
    적절한 status code를 함께 응답

--- routes/member.js
+++ routes/member.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const { sign, verify } = require("../modules/jwt");
 const { isSignedIn, isAdmin } = require("./middlewares");
 const { Member, Post } = require("../models/index");
@@ -8,46 +9,57 @@ const { Member, Post } = require("../models/index");
  * 회원 목록 조회
  */
 router.get("/list", isSignedIn, isAdmin, async (req, res) => {
-  const memberList = await Member.findAll();
-
-  return res.status(200).json({
-    code: 200,
-    data: memberList,
-  });
+  try {
+    const memberList = await Member.findAll();
+    return res.status(StatusCodes.OK).json({
+      data: memberList,
+    });
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+  }
 });
 
 /**
  * 회원 정보 조회
  */
 router.get("/info", isSignedIn, async (req, res) => {
-  const signedinMember = await Member.findOne({
-    where: { member_id: verify(res.locals.token).memberId },
-  });
+  try {
+    const signedinMember = await Member.findOne({
+      where: { member_id: verify(res.locals.token).memberId },
+    });
 
-  const signedinMemberInfo = await Member.findByPk(signedinMember.member_idx, {
-    attributes: ["member_id", "member_nickname"],
-  });
-
-  return res.status(200).json({
-    code: 200,
-    data: signedinMemberInfo,
-  });
+    const signedinMemberInfo = await Member.findByPk(
+      signedinMember.member_idx,
+      {
+        attributes: ["member_id", "member_nickname"],
+      }
+    );
+    return res.status(StatusCodes.OK).json({
+      data: signedinMemberInfo,
+    });
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+  }
 });
 
 /**
  * 회원 정보 수정
  */
 router.patch("/info", isSignedIn, async (req, res) => {
-  const signedinMember = await Member.findOne({
-    where: { member_id: verify(res.locals.token).memberId },
-  });
-
-  const info = {
-    member_id: req.body.id,
-    member_nickname: req.body.nickname,
-  };
-
   try {
+    const signedinMember = await Member.findOne({
+      where: { member_id: verify(res.locals.token).memberId },
+    });
+
+    const info = {
+      member_id: req.body.id,
+      member_nickname: req.body.nickname,
+    };
+
     await Member.update(info, {
       where: { member_idx: signedinMember.member_idx },
     });
@@ -62,15 +74,11 @@ router.patch("/info", isSignedIn, async (req, res) => {
       `token=${token}; Path=/; HttpOnly; SameSite=none; secure=true;`
     );
 
-    return res.status(201).json({
-      code: 201,
-      message: "modify info successfully.",
-    });
+    return res.status(StatusCodes.CREATED).send(ReasonPhrases.CREATED);
   } catch {
-    return res.status(500).json({
-      code: 500,
-      message: "internal server error. please retry.",
-    });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
 });
 
@@ -78,55 +86,59 @@ router.patch("/info", isSignedIn, async (req, res) => {
  * 회원 게시글 조회
  */
 router.get("/posts", isSignedIn, async (req, res) => {
-  const signedinMemberInfo = await Member.findOne({
-    where: { member_id: verify(res.locals.token).memberId },
-  });
+  try {
+    const signedinMemberInfo = await Member.findOne({
+      where: { member_id: verify(res.locals.token).memberId },
+    });
 
-  const posts = await Post.findAll({
-    where: {
-      member_idx: signedinMemberInfo.member_idx,
-    },
-    include: [
-      {
-        model: Member,
-        as: "post_writer",
-        attributes: ["member_id", "member_nickname"],
+    const posts = await Post.findAll({
+      where: {
+        member_idx: signedinMemberInfo.member_idx,
       },
-    ],
-  });
-
-  return res.status(200).json({
-    code: 200,
-    data: posts,
-  });
+      include: [
+        {
+          model: Member,
+          as: "post_writer",
+          attributes: ["member_id", "member_nickname"],
+        },
+      ],
+    });
+    return res.status(StatusCodes.OK).json({
+      data: posts,
+    });
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+  }
 });
 
 /**
  * 회원 탈퇴
  */
 router.delete("/", isSignedIn, async (req, res) => {
-  const signedinMemberInfo = await Member.findOne({
-    where: { member_id: verify(res.locals.token).memberId },
-  });
+  try {
+    const signedinMemberInfo = await Member.findOne({
+      where: { member_id: verify(res.locals.token).memberId },
+    });
 
-  if (signedinMemberInfo) {
-    await Member.destroy({
-      where: { member_idx: signedinMemberInfo.member_idx },
-    });
-    res.setHeader(
-      "Set-Cookie",
-      `token=${res.locals.token}; Path=/; HttpOnly; SameSite=none; secure=true; Max-Age=0`
-    );
+    if (signedinMemberInfo) {
+      await Member.destroy({
+        where: { member_idx: signedinMemberInfo.member_idx },
+      });
+      res.setHeader(
+        "Set-Cookie",
+        `token=${res.locals.token}; Path=/; HttpOnly; SameSite=none; secure=true; Max-Age=0`
+      );
 
-    return res.status(200).json({
-      code: 200,
-      message: "create account successfully.",
-    });
-  } else {
-    return res.status(404).json({
-      code: 404,
-      message: "cannot find account. please retry.",
-    });
+      return res.status(StatusCodes.CREATED).send(ReasonPhrases.CREATED);
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
+    }
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
 });
 

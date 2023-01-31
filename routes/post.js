8c38efd27a6254c000d0808f28630172f1356097
YUZAMIN <untmd.crmn@gmail.com>
2023-02-01 02:59:04 +0900

    :sparkles: post 로직에 try catch문 적용
    적절한 status code와 함께 응답

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const { verify } = require("../modules/jwt");
 const { isSignedIn, isPostWriter, isCommentWriter } = require("./middlewares");
 const { Member, Post, Comment } = require("../models/index");
@@ -8,75 +9,82 @@ const { Member, Post, Comment } = require("../models/index");
  * 게시글 목록 조회
  */
 router.get("/list", async (req, res) => {
-  const postList = await Post.findAll({
-    include: [
-      {
-        model: Member,
-        as: "post_writer",
-        attributes: ["member_id", "member_nickname"],
-      },
-      {
-        model: Comment,
-        as: "comments",
-        attributes: [
-          "comment_idx",
-          "comment_contents",
-          "createdAt",
-          "updatedAt",
-        ],
-      },
-    ],
-  });
-  return res.status(200).json({
-    code: 200,
-    data: postList,
-  });
+  try {
+    const postList = await Post.findAll({
+      include: [
+        {
+          model: Member,
+          as: "post_writer",
+          attributes: ["member_id", "member_nickname"],
+        },
+        {
+          model: Comment,
+          as: "comments",
+          attributes: [
+            "comment_idx",
+            "comment_contents",
+            "createdAt",
+            "updatedAt",
+          ],
+        },
+      ],
+    });
+    return res.status(StatusCodes.OK).json({
+      data: postList,
+    });
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+  }
 });
 
 /**
  * 단일 게시글 조회
  */
 router.get("/:postIdx", async (req, res) => {
-  const postIdx = req.params.postIdx;
+  try {
+    const postIdx = req.params.postIdx;
 
-  const post = await Post.findOne({
-    where: { post_idx: postIdx },
-    include: [
-      {
-        model: Member,
-        as: "post_writer",
-        attributes: ["member_id", "member_nickname"],
-      },
-      {
-        model: Comment,
-        as: "comments",
-        attributes: [
-          "comment_idx",
-          "comment_contents",
-          "createdAt",
-          "updatedAt",
-        ],
-        include: [
-          {
-            model: Member,
-            as: "comment_writer",
-            attributes: ["member_id", "member_nickname"],
-          },
-        ],
-      },
-    ],
-  });
+    const post = await Post.findOne({
+      where: { post_idx: postIdx },
+      include: [
+        {
+          model: Member,
+          as: "post_writer",
+          attributes: ["member_id", "member_nickname"],
+        },
+        {
+          model: Comment,
+          as: "comments",
+          attributes: [
+            "comment_idx",
+            "comment_contents",
+            "createdAt",
+            "updatedAt",
+          ],
+          include: [
+            {
+              model: Member,
+              as: "comment_writer",
+              attributes: ["member_id", "member_nickname"],
+            },
+          ],
+        },
+      ],
+    });
 
-  if (post) {
-    return res.status(200).json({
-      code: 200,
-      data: post,
-    });
-  } else {
-    return res.status(404).json({
-      code: 404,
-      message: "post not found",
-    });
+    if (post) {
+      return res.status(StatusCodes.OK).json({
+        data: post,
+      });
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
+    }
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
 });
 
@@ -84,29 +92,25 @@ router.get("/:postIdx", async (req, res) => {
  * 게시글 생성
  */
 router.post("/", isSignedIn, async (req, res) => {
-  const signedinId = verify(res.locals.token).memberId;
-
-  const postWriter = await Member.findOne({
-    where: { member_id: signedinId },
-  });
-
-  const post = {
-    post_title: req.body.title,
-    post_contents: req.body.contents,
-    member_idx: postWriter.member_idx,
-  };
-
   try {
+    const signedinId = verify(res.locals.token).memberId;
+
+    const postWriter = await Member.findOne({
+      where: { member_id: signedinId },
+    });
+
+    const post = {
+      post_title: req.body.title,
+      post_contents: req.body.contents,
+      member_idx: postWriter.member_idx,
+    };
+
     await Post.create(post);
-    return res.status(201).json({
-      code: 201,
-      message: "create post successfully.",
-    });
+    return res.status(StatusCodes.CREATED).send(ReasonPhrases.CREATED);
   } catch {
-    return res.status(500).json({
-      code: 500,
-      message: "internal server error. please retry.",
-    });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
 });
 
@@ -114,24 +118,20 @@ router.post("/", isSignedIn, async (req, res) => {
  * 게시글 수정
  */
 router.patch("/:postIdx", isSignedIn, isPostWriter, async (req, res) => {
-  const postIdx = req.params.postIdx;
-
-  const post = {
-    post_title: req.body.title,
-    post_contents: req.body.contents,
-  };
-
   try {
+    const postIdx = req.params.postIdx;
+
+    const post = {
+      post_title: req.body.title,
+      post_contents: req.body.contents,
+    };
+
     await Post.update(post, { where: { post_idx: postIdx } });
-    return res.status(201).json({
-      code: 201,
-      message: "modify post successfully.",
-    });
+    return res.status(StatusCodes.CREATED).send(ReasonPhrases.CREATED);
   } catch {
-    return res.status(500).json({
-      code: 500,
-      message: "internal server error. please retry.",
-    });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
 });
 
@@ -139,25 +139,25 @@ router.patch("/:postIdx", isSignedIn, isPostWriter, async (req, res) => {
  * 게시글 삭제
  */
 router.delete("/:postIdx", isSignedIn, isPostWriter, async (req, res) => {
-  const postIdx = req.params.postIdx;
+  try {
+    const postIdx = req.params.postIdx;
 
-  const post = await Post.findOne({
-    where: { post_idx: postIdx },
-  });
-
-  if (post) {
-    await Post.destroy({
+    const post = await Post.findOne({
       where: { post_idx: postIdx },
     });
-    return res.status(200).json({
-      code: 200,
-      message: "delete post successfully.",
-    });
-  } else {
-    return res.status(404).json({
-      code: 404,
-      message: "cannot find post. please retry.",
-    });
+
+    if (post) {
+      await Post.destroy({
+        where: { post_idx: postIdx },
+      });
+      return res.status(StatusCodes.OK).send(ReasonPhrases.OK);
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
+    }
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
 });
 
@@ -165,21 +165,19 @@ router.delete("/:postIdx", isSignedIn, isPostWriter, async (req, res) => {
  * 특정 게시글 댓글 조회
  */
 router.get("/:postIdx/comment/list", async (req, res) => {
-  const postIdx = req.params.postIdx;
-
   try {
+    const postIdx = req.params.postIdx;
+
     const commentList = await Comment.findAll({
       where: { post_idx: postIdx },
     });
-    return res.status(200).json({
-      code: 200,
+    return res.status(StatusCodes.OK).json({
       data: commentList,
     });
   } catch {
-    return res.status(500).json({
-      code: 500,
-      message: "internal server error. please retry.",
-    });
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
   }
 });
 
@@ -187,24 +185,27 @@ router.get("/:postIdx/comment/list", async (req, res) => {
  * 댓글 생성
  */
 router.post("/:postIdx/comment", isSignedIn, async (req, res) => {
-  const postIdx = req.params.postIdx;
-  const signedinId = verify(res.locals.token).memberId;
+  try {
+    const postIdx = req.params.postIdx;
+    const signedinId = verify(res.locals.token).memberId;
 
-  const commentWriter = await Member.findOne({
-    where: { member_id: signedinId },
-  });
+    const commentWriter = await Member.findOne({
+      where: { member_id: signedinId },
+    });
 
-  const comment = {
-    comment_contents: req.body.contents,
-    member_idx: commentWriter.member_idx,
-    post_idx: postIdx,
-  };
+    const comment = {
+      comment_contents: req.body.contents,
+      member_idx: commentWriter.member_idx,
+      post_idx: postIdx,
+    };
 
-  await Comment.create(comment);
-  return res.status(201).json({
-    code: 201,
-    message: "create comment successfully.",
-  });
+    await Comment.create(comment);
+    return res.status(StatusCodes.CREATED).send(ReasonPhrases.CREATED);
+  } catch {
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+  }
 });
 
 /**
@@ -215,23 +216,19 @@ router.patch(
   isSignedIn,
   isCommentWriter,
   async (req, res) => {
-    const commentIdx = req.params.commentIdx;
-
-    const comment = {
-      comment_contents: req.body.contents,
-    };
-
     try {
+      const commentIdx = req.params.commentIdx;
+
+      const comment = {
+        comment_contents: req.body.contents,
+      };
+
       await Comment.update(comment, { where: { comment_idx: commentIdx } });
-      return res.status(201).json({
-        code: 201,
-        message: "modify comment successfully.",
-      });
+      return res.status(StatusCodes.CREATED).send(ReasonPhrases.CREATED);
     } catch {
-      return res.status(500).json({
-        code: 500,
-        message: "internal server error. please retry.",
-      });
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
     }
   }
 );
@@ -244,24 +241,24 @@ router.delete(
   isSignedIn,
   isCommentWriter,
   async (req, res) => {
-    const commentIdx = req.params.commentIdx;
-    const comment = await Comment.findOne({
-      where: { comment_idx: commentIdx },
-    });
-
-    if (comment) {
-      await Comment.destroy({
+    try {
+      const commentIdx = req.params.commentIdx;
+      const comment = await Comment.findOne({
         where: { comment_idx: commentIdx },
       });
-      return res.status(200).json({
-        code: 200,
-        message: "delete comment successfully.",
-      });
-    } else {
-      return res.status(404).json({
-        code: 404,
-        message: "cannot find comment. please retry.",
-      });
+
+      if (comment) {
+        await Comment.destroy({
+          where: { comment_idx: commentIdx },
+        });
+        return res.status(StatusCodes.OK).send(ReasonPhrases.OK);
+      } else {
+        return res.status(StatusCodes.NOT_FOUND).send(ReasonPhrases.NOT_FOUND);
+      }
+    } catch {
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
     }
   }
 );

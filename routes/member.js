7bb1e65b72561d397ba07241fe411b5edc8e0149
YUZAMIN <untmd.crmn@gmail.com>
2023-02-10 03:56:38 +0900

    :bug: sequelize 모듈 import문 빼먹은 오류

--- routes/member.js
+++ routes/member.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Sequelize = require("sequelize");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const { sign, verify } = require("../modules/jwt");
 const { isSignedIn, isAdmin } = require("./middlewares");
